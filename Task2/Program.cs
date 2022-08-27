3f4980f20fda2d2ed339d198a62c4fe33d816d5e
Veronika <griva.verona@gmail.com>
2022-08-28 00:38:32 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string xText = Convert.ToString(x);
+if ( xText.Length > 2)
+    Console.WriteLine("Третья цифра: " + xText[2]);
+
+else 
+    Console.WriteLine("Третьей цифры нет");
+// 
