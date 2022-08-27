d05efbe0e1474b31f6da27c4a4867c8795d6099b
Veronika <griva.verona@gmail.com>
2022-08-28 00:13:32 +0300

    add text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,7 +3,7 @@
 
 Console.WriteLine("введите число: ");
 int x = Convert.ToInt32(Console.ReadLine());
-int a1 = x / 10;
-int a2 = a1 % 10;
-Console.WriteLine(a2);
+int a = x / 10;
+a = a % 10;
+Console.WriteLine("Вторая цифра: "  + a);
 
